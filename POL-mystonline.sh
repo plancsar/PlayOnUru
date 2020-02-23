cea30e73f6d4d71a9121a53bf5354995b11bcbf7
plancsar <plancsar@users.noreply.github.com>
2020-02-23 15:40:54 +0000

    Update POL-mystonline.sh

--- POL-mystonline.sh
+++ POL-mystonline.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env playonlinux-bash
-# Date: 2018-01-30 19:50 GMT
+# Date: 2020-02-23
 # Wine version used: 3.0
-# Distribution used to test: Lubuntu 17.10 64 (VirtualBox)
+# Distribution used to test: Lubuntu 18.04 LTS 64bit (VirtualBox)
 # Author: Korovev
 
 [ "$PLAYONLINUX" = "" ] && exit 0
@@ -9,7 +9,6 @@ source "$PLAYONLINUX/lib/sources"
 
 TITLE="Myst Online: Uru Live (again)"
 PREFIX="mystonline"
-
 
 POL_SetupWindow_Init
 POL_SetupWindow_presentation "$TITLE" "Cyan, Inc." "http://mystonline.com" "Korovev" "$PREFIX"
@@ -22,17 +21,16 @@ POL_Wine_PrefixCreate "3.0"
 POL_System_TmpCreate "$PREFIX"
 cd "$POL_System_TmpDir"
 
-
 # Installing components
 POL_Call POL_Install_vcrun6
-POL_Call POL_Install_physx
-
+POL_Download "https://raw.githubusercontent.com/plancsar/PlayOnUru/master/PhysX_Setup.exe"
+POL_Wine PhysX_Setup.exe
+POL_Wine_WaitExit "$TITLE"
 
 # Download & Install the game
 POL_Download "http://account.mystonline.com/download/MOULInstaller.exe"
 POL_Wine MOULInstaller.exe
 POL_Wine_WaitExit "$TITLE"
-
 
 # Cleanup
 POL_System_TmpDelete
