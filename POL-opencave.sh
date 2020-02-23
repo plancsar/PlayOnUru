2fd63f70ec408736ef239677a3aef989caf009fc
plancsar <plancsar@users.noreply.github.com>
2020-02-23 15:43:55 +0000

    Update POL-opencave.sh

--- POL-opencave.sh
+++ POL-opencave.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env playonlinux-bash
-# Date: 2018-01-30 19:50 GMT
+# Date: 2020-02-23
 # Wine version used: 3.0
-# Distribution used to test: Lubuntu 17.10 64 (VirtualBox)
+# Distribution used to test: Lubuntu 18.04 LTS 64bit (VirtualBox)
 # Author: Korovev
 
 [ "$PLAYONLINUX" = "" ] && exit 0
@@ -9,7 +9,6 @@ source "$PLAYONLINUX/lib/sources"
 
 TITLE="The Open Cave"
 PREFIX="toc_moul"
-
 
 POL_SetupWindow_Init
 POL_SetupWindow_presentation "$TITLE" "$TITLE" "http://the-open-cave.net/en/" "Korovev" "$PREFIX"
@@ -21,11 +20,8 @@ POL_Wine_PrefixCreate "3.0"
 POL_System_TmpCreate "$PREFIX"
 cd "$POL_System_TmpDir"
 
-
 # Installing components
 POL_Call POL_Install_vcrun2013
-POL_Call POL_Install_physx
-
 
 # If MO:ULa is already installed in $MOULAPATH, copy the datafiles from there
 if [ -d $HOME/Library/PlayOnMac ]; then
@@ -42,17 +38,6 @@ if [ "$(POL_Wine_PrefixExists 'mystonline')" = "True" ]; then
     mkdir "$SHARDPATH"
     POL_SetupWindow_wait "Copying data from the Myst Online prefix..." "$TITLE"
     cp -r "$MOULAPATH"/dat "$MOULAPATH"/sfx "$MOULAPATH"/avi "$SHARDPATH"/
-
-# If MO:ULa is not installed, download and launch the installer
-# else
-#     POL_Download "http://account.mystonline.com/download/MOULInstaller.exe"
-#     POL_Wine MOULInstaller.exe
-#     POL_Wine_WaitExit "$TITLE"
-
-#     Rename the Uru Live folder to "TOC-Moul", to let the installer recognize it
-#     mv "$RENAMPATH" "$SHARDPATH"
-fi
-
 
 # Install the game
 POL_Download "http://other.the-open-cave.net/installer/TOC-Moul.exe"
