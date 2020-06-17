9a4b21f29c7a15c968a5c3655efb2d6b1d7118df
plancsar <plancsar@users.noreply.github.com>
2020-06-17 14:47:54 +0100

    Revert "Delete POL-deepisland.sh"
    This reverts commit c2aac52fe1e156090a01b30c8be05da8152eb927.

---
+++ POL-deepisland.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env playonlinux-bash
+# Date: 2018-01-31 23:00 GMT
+# Wine version used: 3.0
+# Distribution used to test: Lubuntu 17.10 64 (VirtualBox)
+# Author: Korovev
+
+[ "$PLAYONLINUX" = "" ] && exit 0
+source "$PLAYONLINUX/lib/sources"
+
+TITLE="Deep Island"
+PREFIX="deepisland"
+
+
+POL_SetupWindow_Init
+POL_SetupWindow_presentation "$TITLE" "Guild of Writers" "https://forum.guildofwriters.org/viewforum.php?f=111" "Korovev" "$PREFIX"
+
+POL_Wine_SelectPrefix "$PREFIX"
+POL_System_SetArch "x86"
+POL_Wine_PrefixCreate "3.0"
+
+POL_System_TmpCreate "$PREFIX"
+cd "$POL_System_TmpDir"
+
+
+# Installing components
+POL_Call POL_Install_vcrun6
+
+
+# Ask for the location of the GOG installer and launch it
+POL_SetupWindow_browse "Please select the Windows GOG installer." "$TITLE"
+POL_Wine $APP_ANSWER
+POL_Wine_WaitExit "$TITLE"
+
+SHARDPATH="$POL_USER_ROOT/wineprefix/$PREFIX/drive_c/GOG Games/Myst Uru Complete Chronicles"
+
+# Patch the game for use with Deep Island
+cd "$SHARDPATH"/
+POL_Download "http://www.the-deep-island.de/shard/di-patcher.zip"
+unzip di-patcher.zip
+rm di-patcher.zip
+
+
+# Cleanup
+POL_System_TmpDelete
+POL_Shortcut "Uru.exe" "$TITLE"
+POL_SetupWindow_Close
+exit
