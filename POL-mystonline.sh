7054f6a4ac9aeb407ccf8361ad14c613de7926e4
plancsar <plancsar@users.noreply.github.com>
2020-08-20 00:35:46 +0100

    Install d3dx10 and vcrun2012 with winetricks

--- POL-mystonline.sh
+++ POL-mystonline.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env playonlinux-bash
-# Date: 2020-06-03
+# Date: 2020-08-19
 # Wine version used: 3.0
 # Distribution used to test: macOS 10.14.6 "Mojave" & Lubuntu 18.04 LTS 64bit (VirtualBox)
 # Author: Korovev
@@ -22,11 +22,13 @@ POL_System_TmpCreate "$PREFIX"
 cd "$POL_System_TmpDir"
 
 # Installing components
-POL_Call POL_Install_vcrun6
+POL_Call POL_Install_d3dx10
+POL_Call POL_Install_vcrun2012
+POL_Call POL_Install_crypt32
 POL_Wine_WaitExit "$TITLE"
 
 # Download & Install the game
-POL_Download "http://account.mystonline.com/download/MOULInstaller.exe"
+curl -L -O "http://account.mystonline.com/download/MOULInstaller.exe"
 POL_Wine MOULInstaller.exe
 POL_Wine_WaitExit "$TITLE"
 
