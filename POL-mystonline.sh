10e8f1bc05b701741bc0e0978632c1798685ad2f
plancsar <plancsar@users.noreply.github.com>
2021-01-13 17:06:53 +0100

    Update to Wine 5.7

--- POL-mystonline.sh
+++ POL-mystonline.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env playonlinux-bash
-# Date: 2020-08-19
-# Wine version used: 3.0
-# Distribution used to test: macOS 10.14.6 "Mojave" & Lubuntu 18.04 LTS 64bit (VirtualBox)
+# Date: 2021-01-13
+# Wine version used: 5.7
+# Distribution used to test: macOS 10.14.5 "Mojave" & Lubuntu 20.04 LTS 64bit
 # Author: Korovev
 
 [ "$PLAYONLINUX" = "" ] && exit 0
@@ -15,17 +15,16 @@ POL_SetupWindow_presentation "$TITLE" "Cyan, Inc." "http://mystonline.com" "Koro
 
 POL_Wine_SelectPrefix "$PREFIX"
 POL_System_SetArch "x86"
-POL_Wine_PrefixCreate "3.0"
-#Set_Desktop "On" "1024" "768"
+POL_Wine_PrefixCreate "5.7"
 
 POL_System_TmpCreate "$PREFIX"
 cd "$POL_System_TmpDir"
 
 # Installing components
-POL_Call POL_Install_d3dx10
-POL_Call POL_Install_vcrun2012
-POL_Call POL_Install_crypt32
-POL_Wine_WaitExit "$TITLE"
+#POL_Call POL_Install_d3dx10
+#POL_Call POL_Install_vcrun2012
+#POL_Call POL_Install_crypt32
+#POL_Wine_WaitExit "$TITLE"
 
 # Download & Install the game
 curl -k -L -O "http://account.mystonline.com/download/MOULInstaller.exe"
